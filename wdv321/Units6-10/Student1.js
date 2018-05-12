4d14a220b2bd8fd29cd4bc852b85e39344ed3371
pickjasmine <francois.jasminem@gmail.com>
2018-05-11 22:14:45 -0500

    Removed homework files

--- wdv321/Units6-10/Student1.js
+++
@@ -1,1 +0,0 @@
-{"student_id":"900006361","student_first_name":"Bryan","student_last_name":"Waters","student_major":"GRD","student_credits":16,"student_gpa":"3.6","student_registration_date":"2017-08-14"}
