b9899c8aa26646b1f001bc00ea60288795a2d1bb
Jasmine Francois <francois.jasminem@gmail.com>
2019-11-30 14:59:04 -0600

    Testing deploying react

---
+++ test-todo-list/precache-manifest.c1fc401f0fb1628d2f81ed76bd65e423.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6b78929b6372a1505719ffd549d879e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cb4463d6fdba91abdd1",
+    "url": "/static/css/main.71be5f0e.chunk.css"
+  },
+  {
+    "revision": "394798a7971e9c73f8ee",
+    "url": "/static/js/2.5495057f.chunk.js"
+  },
+  {
+    "revision": "9cb4463d6fdba91abdd1",
+    "url": "/static/js/main.1027e3b7.chunk.js"
+  },
+  {
+    "revision": "1d3b7e3b5576d3d21ce2",
+    "url": "/static/js/runtime-main.5f5a7bd9.js"
+  }
+]);
