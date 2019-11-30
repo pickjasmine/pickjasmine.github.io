64acb28224faf5ca0afd1e98a230ad5cc1a68386
Jasmine Francois <francois.jasminem@gmail.com>
2019-11-30 15:01:34 -0600

    Attempt #2

---
+++ test-todo-list/precache-manifest.3dc9168a90feb5c6cacf90c87119e1f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c16d4e196a4ae4943e67e4818e7a0617",
+    "url": "/test-todo-list/index.html"
+  },
+  {
+    "revision": "9cb4463d6fdba91abdd1",
+    "url": "/test-todo-list/static/css/main.71be5f0e.chunk.css"
+  },
+  {
+    "revision": "394798a7971e9c73f8ee",
+    "url": "/test-todo-list/static/js/2.5495057f.chunk.js"
+  },
+  {
+    "revision": "9cb4463d6fdba91abdd1",
+    "url": "/test-todo-list/static/js/main.1027e3b7.chunk.js"
+  },
+  {
+    "revision": "d8ebe49e7122d50b7838",
+    "url": "/test-todo-list/static/js/runtime-main.20832af5.js"
+  }
+]);
